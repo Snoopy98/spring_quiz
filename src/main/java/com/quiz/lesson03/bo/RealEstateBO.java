4fe874e7278bdd0d55380f85ae727e6513233190
Test <snoopy980306>
2023-06-16 18:40:03 +0900

    마지막 문제 풀어오기

---
+++ src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
@@ -0,0 +1,29 @@
+package com.quiz.lesson03.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quiz.lesson03.dao.RealEstateMapper;
+import com.quiz.lesson03.domain.RealEstate;
+
+@Service
+public class RealEstateBO {
+@Autowired
+
+//input : id
+//output: RealEstate => 컨트롤러한테 
+	private RealEstateMapper realEstateMapper;
+
+	public RealEstate getRealEstateById(int id) {
+		return realEstateMapper.selectRealEstateById(id);
+	}
+	
+	//input : rentPrice
+	// output: List<RealEstate>
+	
+	public List<RealEstate> getRealEstateByRentPrice(int rentPrice) {
+		return realEstateMapper.selectRealEstateByRentPrice(rentPrice);
+	}
+}
---
+++ src/main/java/com/quiz/lesson03/dao/RealEstateMapper.java
@@ -0,0 +1,15 @@
+package com.quiz.lesson03.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.quiz.lesson03.domain.RealEstate;
+@Repository
+public interface RealEstateMapper {
+
+	public RealEstate selectRealEstateById(@Param("id") int id); // @Param: 파람안에 넣는 명으로 xml에 사용된다. 생략 가능하지만 두개 이상일때는 필수 
+	
+	public List<RealEstate> selectRealEstateByRentPrice(@Param("rentPrice") int rentPrice);
+}
